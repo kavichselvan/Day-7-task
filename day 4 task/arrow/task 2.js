cdf74c52ebbf2e5d2193f7564eb156bf4e20a353
Kavichselvan S <116435403+kavichselvan@users.noreply.github.com>
2023-08-13 17:05:19 +0530

    Delete day 4 task directory
    https://docs.google.com/document/d/1VyZJG0KZAoZEs3h_lpP65XPekNqsbFp8bouOZSWsOTc/edit

--- day 4 task/arrow/task 2.js
+++
@@ -1,9 +0,0 @@
-//Convert all the strings to title caps in a string array
-
-const convertToTitleCaps = (arr) => {
-  return arr.map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase());
-};
-
-const strings = ["hello", "world", "javascript", "programming"];
-const titleCapsStrings = convertToTitleCaps(strings);
-console.log(titleCapsStrings);
--- day 4 task/arrow/task 3.js
+++
@@ -1,7 +0,0 @@
-//Sum of all numbers in an array
-
-const sumArray = (arr) => arr.reduce((sum, num) => sum + num, 0);
-
-const numbers = [1, 2, 3, 4, 5];
-const sum = sumArray(numbers);
-console.log(sum);
